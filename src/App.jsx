cda0850e9df7e90cff83e519e74224a060a17366
elianmtg <elianmtoribio@gmail.com>
2023-02-02 18:33:20 -0400

    fix: change files js to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
-import reactLogo from './assets/react.svg'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
 
 function App() {
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    // <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route
+        path="*"
+        element={<Home/>}
+      />
+    </Routes>
+    // <Footer/>
   )
 }
 
