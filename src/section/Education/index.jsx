e4f124e9476ac9463d2956d3017ea1c5155d7f15
elianmtg <elianmtoribio@gmail.com>
2023-02-09 13:25:50 -0400

    style(section): add timeline for education

--- src/section/Education/index.jsx
+++ src/section/Education/index.jsx
@@ -1,7 +1,15 @@
+import Timeline from "../../components/Timeline"
+import itla from '../../assets/images/Enterprises/Itla.webp'
+
 export default function Education(){
     return(
         <section>
-            <h2>Education</h2>
+            <h2>Work Experience</h2>
+            <Timeline 
+                name="Software Developer" enterprise="Instituto Tecnológico de las Américas (ITLA)" logo={itla} 
+                startDate="January 2020" endDate="April 2022" 
+                summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            />
         </section>
     )
 }
--- src/section/WorkExperience/index.jsx
+++ src/section/WorkExperience/index.jsx
@@ -1,5 +1,5 @@
 import Timeline from "../../components/Timeline"
-import cero from '../../assets/images/Enterprises/Itla.webp'
+import cero from '../../assets/images/Enterprises/Cerobucks.webp'
 
 export default function WorkExperience(){
     return(
@@ -7,7 +7,7 @@ export default function WorkExperience(){
             <h2>Work Experience</h2>
             <Timeline 
                 name="Front-End Developer" enterprise="Cerobucks, S.R.L" logo={cero} 
-                startDate="Enero 2022" endDate="Marzo 2022" 
+                startDate="January 2022" endDate="March 2022" 
                 summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
             />
         </section>
