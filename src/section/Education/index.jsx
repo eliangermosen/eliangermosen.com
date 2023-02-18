80c5d33dfa5348ac87d163c7f9d885efb63e51f5
elianmtg <elianmtoribio@gmail.com>
2023-02-18 13:44:29 -0400

    styles: chips in mobile responsive

--- src/section/Education/index.jsx
+++ src/section/Education/index.jsx
@@ -8,7 +8,10 @@ export default function Education(){
             <Timeline 
                 name="Software Developer" enterprise="Instituto Tecnológico de las Américas (ITLA)" logo={itla} 
                 startDate="January 2020" endDate="April 2022" 
-                summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                summary="During my proccess of education in this institute I worked with a team of developers where collaboratively
+                we created projects from scratch to production wich I could improve my habilities of comunication, leadership and teamwork.
+                Besides for my continous learning I've taken courses and done projects alone, for the same way take my knowledge to the 
+                highest level and could have the hability to solve problems."
             />
         </section>
     )
