9ead9f3b272c985a836bc75a1c54091fc8684310
elianmtg <elianmtoribio@gmail.com>
2024-08-05 13:34:18 -0400

    feat(workexperience): add new exp

--- src/section/WorkExperience/index.jsx
+++ src/section/WorkExperience/index.jsx
@@ -6,13 +6,17 @@ export default function WorkExperience(){
     return(
         <section>
             <h2>Work Experience</h2>
-            {dataWorkExperience.map((work) => (
-                <Timeline 
-                    key={work.id} name={work.title} enterprise={work.enterprise}
-                    logo={work.image} startDate={`${work.date.start.month} ${work.date.start.year}`} endDate={`${work.date.end.month} ${work.date.end.year}`}
-                    summary={work.summary}
-                />
-            ))}
+            {
+                dataWorkExperience
+                .sort((a, b) => b.order - a.order)
+                .map((work) => (
+                    <Timeline 
+                        key={work.id} name={work.title} enterprise={work.enterprise}
+                        logo={work.image} startDate={`${work.date.start.month} ${work.date.start.year}`} endDate={`${work.date.end.month} ${work.date.end.year}`}
+                        summary={work.summary}
+                    />
+                ))
+            }
         </section>
     )
 }
--- src/services/data/WorkExperience.js
+++ src/services/data/WorkExperience.js
@@ -1,8 +1,10 @@
 import Cerobucks from '../../assets/images/Enterprises/Cerobucks.webp'
+import Mapre from '../../assets/images/Enterprises/MAPRE.webp'
 
 const WorkExperience = [
     {
         "id": 1,
+        "order": 1,
         "image": Cerobucks,
         "title": "Front-End Developer",
         "enterprise": "Cerobucks, S.R.L",
@@ -17,6 +19,24 @@ const WorkExperience = [
             }
         },
         "summary": "Modernize the company website. Improve the user experience. The Stack used in this experience was React, agile SCRUM methodologies adapted to completely remote work."
+    },
+    {
+        "id": 2,
+        "order": 2,
+        "image": Mapre,
+        "title": "Web Developer",
+        "enterprise": "Ministerio Administrativo de la Presidencia (MAPRE)",
+        "date": {
+            "start": {
+                "year": 2023,
+                "month": "April"
+            },
+            "end": {
+                "year": 2024,
+                "month": "May"
+            }
+        },
+        "summary": "Responsible for overseeing the transition of projects from the design stage to implementation and system development. My role encompasses creating intuitive UX/UI designs to ensure an engaging user experience, developing responsive frontend applications using React and TypeScript, and optimizing backend processes with Web API Net Core to enhance business logic, data management, and security, have the best performance."
     }
 ]
 
